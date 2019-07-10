61ec06a18e7b773d14efd4c125e965598347fd86
Dominik Woiwode <mail@dominik-woiwode.de>
2019-07-10 13:46:29 +0200

    Add RLE Loading

--- utils/boardIO.py
+++ utils/boardIO.py
@@ -1,10 +1,13 @@
 import os
+import re
 from pathlib import Path
 
 import cv2
 import numpy as np
 
 # Default chars used when saving boards
+from tqdm import tqdm
+
 CHAR_ON = "X"
 CHAR_OFF = "_"
 CHAR_DELIMITER = "\n"
@@ -26,6 +29,9 @@ def saveBoard(board, filename, on=CHAR_ON, off=CHAR_OFF, delimiter=CHAR_DELIMITE
         )
         for y in range(len(board[0]))
     )
+
+    if "." not in filename:
+        filename += ".board"
     os.makedirs(Path(filename).parent, exist_ok=True)
     with open(filename, "w") as d:
         d.write(off + on + delimiter)
@@ -33,11 +39,8 @@ def saveBoard(board, filename, on=CHAR_ON, off=CHAR_OFF, delimiter=CHAR_DELIMITE
 
 
 def loadBoard(filename):
-    if not os.path.exists(filename):
-        if os.path.exists(filename + ".board"):
-            filename += ".board"
-        else:
-            raise FileNotFoundError
+    filename = _checkFileExists(filename, ".board")
+
     with open(filename, "r") as d:
         off, on, delimiter = d.read(3)
         data = [[c for c in row] for row in d.read().split(delimiter) if row.strip()]
@@ -50,7 +53,7 @@ def loadBoard(filename):
             elif data[y][x] == off:
                 board[x][y] = 0
             else:
-                print(f"Invalid char at ({x},{y}): {data[x][y]}")
+                raise ValueError(f"Invalid char at ({x},{y}): {data[x][y]}")
     return board
 
 
@@ -68,6 +71,8 @@ def saveCompressedBoard(board, filename):
     for i in range(len(singleBinString) // 8):
         s += chr(int(singleBinString[i * 8:(i + 1) * 8], 2))
 
+    if "." not in filename:
+        filename += ".boardC"
     os.makedirs(Path(filename).parent, exist_ok=True)
     with open(filename, "wb") as d:
         w = len(board).to_bytes(byteorder="big", length=COMPRESSED_DIM_LEN)
@@ -78,11 +83,7 @@ def saveCompressedBoard(board, filename):
 
 
 def loadCompressedBoard(filename):
-    if not os.path.exists(filename):
-        if os.path.exists(filename + ".boardC"):
-            filename += ".boardC"
-        else:
-            raise FileNotFoundError
+    filename = _checkFileExists(filename, ".boardC")
 
     with open(filename, "rb") as d:
         widthBin = d.read(COMPRESSED_DIM_LEN)
@@ -104,6 +105,62 @@ def loadCompressedBoard(filename):
             y = pos // width
             board[x][y] = int(val)
             pos += 1
+    return board
+
+
+# === RLE ===
+# Run-length encoded. Simple Algorithm used by golly
+# Run-length encoding is a simple (but not very efficient) method of file compression.
+# In Life the term refers to a specific ASCII encoding used for patterns in Conway's Life and other similar cellular automata.
+# This encoding was introduced by Dave Buckingham and is now the usual means of exchanging relatively
+# small patterns by email or in online forum discussions.
+# The "run lengths" are the numbers, b's are dead cells, o's are live cells, and dollar signs signal new lines:
+def saveRLE(board, filename):
+    pass
+
+
+def loadRLE(filename):
+    filename = _checkFileExists(filename, ".rle")
+    with open(filename, "r") as d:
+        line = "#"
+        while line.startswith("#"):
+            line = d.readline()   # Reads one more line than needed. This line contains position and rules
+        data = d.read()
+
+    data = data.replace("\n", "")  # Linebreaks are irrelevant
+    data = data.split("!")[0]  # End of file
+    lines = data.split("$")
+    del data
+    countings = [map(lambda s: int(s) if s != "" else 1, re.split("[ob]", line)) for line in lines]
+    w = max([sum(row) for row in countings])
+    h = len(countings)
+    del countings
+
+    print("Width:", w, "Height:", h)
+    board = emptyBoard(w, h)
+    alreadyParsedLines = dict()
+    for i, line in tqdm(enumerate(lines),desc="Loading RLE", total=h):
+        if line in alreadyParsedLines:
+            board[:, i] = alreadyParsedLines[line]
+            # print("Used Cache (",i,")")
+            continue
+
+        # if not i % 5000:
+        #     print(f"RLE Loading line: {i}/{h} ({i / h * 100:.2f}%)")
+        parsedRow = np.array([0 for _ in range(w)])
+        curN = ""
+        sumRow = 0
+        for c in line:
+            if c == "o" or c == "b":
+                n = int(curN) if curN != "" else 1
+                if c == "o":
+                    parsedRow[sumRow:sumRow + n] = 1
+                sumRow += n
+                curN = ""
+                continue
+            curN += c
+        alreadyParsedLines[line] = parsedRow
+        board[:, i] = parsedRow
     return board
 
 
@@ -132,15 +189,15 @@ def fromImage(path, pixelPerCell=1, threshold=128):
 
 
 # ===== Static Functions =====
+def emptyBoard(width, height):
+    return np.zeros((width, height))
+
+
 def createRandomBoard(width, height, rndThreshold=0.5):
-    board = np.zeros((width, height))
+    board = emptyBoard(width, height)
     rnd = np.random.random((width, height))
     board[np.where(rnd < rndThreshold)] = 1
     return board
-
-
-def emptyBoard(width, height):
-    return np.zeros((width, height))
 
 
 def checkEquals(board1, board2):
@@ -148,10 +205,13 @@ def checkEquals(board1, board2):
 
 
 def addBorder(board, borderSize):
-    w = len(board)
-    h = len(board[0])
-    w += 2 * borderSize
-    h += 2 * borderSize
-    newBoard = np.zeros((w, h))
-    newBoard[borderSize:w - borderSize, borderSize:h - borderSize] = board
-    return newBoard
+    return np.pad(board, pad_width=borderSize, mode='constant', constant_values=0)
+
+
+def _checkFileExists(filename, ending):
+    if not os.path.exists(filename):
+        if os.path.exists(filename + ending):
+            filename += ending
+        else:
+            raise FileNotFoundError
+    return filename
